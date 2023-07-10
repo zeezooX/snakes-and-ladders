a4306c32475937fbfe5a5fafda6eef72715b00c9
Mo'men Gharib <momengharib22@hotmail.com>
2023-07-10 23:28:41 +0300

    login and register pages (ui)

--- client/src/pages/loginPage/Login.jsx
+++ client/src/pages/loginPage/Login.jsx
@@ -1,23 +1,35 @@
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="container">
-      <div className="card">
-        <div className="left">
-          <h1>Welcome to Snakes and Ladders</h1>
-          <p className="text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-            omnis, vero assumenda nobis cum accusantium consectetur! Accusantium
-            velit ex eligendi quia nesciunt blanditiis, explicabo alias aliquam
-            quod consequatur deserunt beatae!
-          </p>
-          <span className="reg">Don't have an account?</span>
-          <button className="register">Register</button>
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="left">
+            <h1>Snakes and Ladders</h1>
+            <p className="text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
+              omnis, vero assumenda nobis cum accusantium consectetur!
+              Accusantium velit ex eligendi quia nesciunt blanditiis, explicabo
+              alias aliquam quod consequatur deserunt beatae!
+            </p>
+            <span className="reg">Don't have an account?</span>
+            <Link to="/register">
+              <button className="register">Register</button>
+            </Link>
+          </div>
+          <div className="right">
+            <h1 className="login">Login</h1>
+            <form>
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <button>login</button>
+            </form>
+          </div>
         </div>
-        <div className="right">hello</div>
       </div>
-    </div>
+    </>
   );
 };
 export default Login;
--- client/src/pages/registerPage/Register.jsx
+++ client/src/pages/registerPage/Register.jsx
@@ -1,3 +1,35 @@
-export default function Register() {
-  return <div>Register</div>;
-}
+import "./Register.css";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <>
+      <div className="Register">
+        <div className="card">
+          <div className="left">
+            <h1>Snakes and Ladders</h1>
+            <p className="text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
+              omnis, vero assumenda nobis cum accusantium consectetur!
+              Accusantium velit ex eligendi quia nesciunt blanditiis, explicabo
+              alias aliquam quod consequatur deserunt beatae!
+            </p>
+            <span className="log">Do you have an account?</span>
+            <Link to="/login">
+              <button className="login">Login</button>
+            </Link>
+          </div>
+          <div className="right">
+            <h1 className="register">Register</h1>
+            <form>
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <button>register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Register;
