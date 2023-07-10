faa8d9b429175b4ccfb6e31d9c0a69f83314959a
unknown <sherin.reda15@gmail.com>
2023-07-10 16:29:31 +0300

    leave-game-updated

--- server/app/routes/RoutesHandlers/handleLeaveGame.js
+++ server/app/routes/RoutesHandlers/handleLeaveGame.js
@@ -4,17 +4,26 @@ const Game = db.Game;
 const handleLeaveGame = async (req, res, next) => {
   const { playerId, gameId } = req.body;
   try {
-    let player = GamePlayer.findOne({ where: { playerId, gameId } });
+    let player = await GamePlayer.findOne({ where: { playerId, gameId } });
     if (!player) throw new Error("Player Not in The Game");
     let game = await Game.findByPk(gameId);
     if (!game) throw new Error("Game Doesn't Exist");
-    GamePlayer.destroy({ where: { playerId } });
     let num = await Game.update(
       { playesNumber: game.playesNumber - 1 },
       { where: { Id: gameId } }
     );
-    console.log(num);
     if (num != 1) throw new Error("Can't Update Game");
+    await GamePlayer.destroy({ where: { playerId } });
+    let gamePlayers = await GamePlayer.findAll({ where: { gameId } });
+    gamePlayers.forEach((playerr) => {
+      console.log(playerr.order, player.order);
+      if (playerr.order > player.order) {
+        GamePlayer.update(
+          { order: playerr.order - 1 },
+          { where: { Id: gameId, playerId: playerr.playerId } }
+        );
+      }
+    });
     res.status(200).send("Done");
   } catch (e) {
     next(e);
