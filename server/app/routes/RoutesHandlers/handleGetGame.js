dcb8476ff3ce9eb418c5cf5bb0f267a3e88c9026
Ziyad Eslam <ziyadeslam@hotmail.com>
2023-07-10 15:41:10 +0300

    getgame final

--- server/app/routes/RoutesHandlers/handleGetGame.js
+++ server/app/routes/RoutesHandlers/handleGetGame.js
@@ -3,16 +3,33 @@ const Game = db.Game;
 const Op = db.Sequelize.Op;
 
 const handleGetGame = (req, res) => {
-  //TODO: Get all users in the game
-  Game.findOne({ where: {Id: req.query.id} })
-    .then((data) => {
-      res.send(data);
+  Game.findOne({ where: { Id: req.query.id } })
+    .then((data1) => {
+      db.sequelize
+        .query(
+          "select * from gameplayer gp join users u on gp.playerId = u.userId where gp.gameId = " +
+            req.query.id +
+            ";",
+          {
+            type: db.sequelize.QueryTypes.SELECT,
+          }
+        )
+        .then((data2) => {
+          res.send({ game: data1, users: data2 });
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while retrieving games.",
+          });
+        });
     })
     .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving games.",
+        message: err.message || "Some error occurred while retrieving games.",
       });
+      flag = false;
+      console.log(flag);
     });
 };
 
