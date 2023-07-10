c5fd81f8521898ba779be05223654a40fbadaac9
Mo'men Gharib <momengharib22@hotmail.com>
2023-07-10 12:35:32 +0300

    loginPage (not complete yet)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,38 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/loginPage/Login";
+import Register from "./pages/registerPage/Register";
+import Home from "./pages/homePage/Home";
+
+const user = false;
 function App() {
-  return <div>hello</div>;
+  const ProtectedRoute = ({ children }) => {
+    if (!user) {
+      return <Navigate to="/login" />;
+    } else return children;
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- client/src/pages/loginPage/Login.jsx
+++ client/src/pages/loginPage/Login.jsx
@@ -1,3 +1,23 @@
-export default function Login() {
-  return <div>Login</div>;
-}
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome to Snakes and Ladders</h1>
+          <p className="text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
+            omnis, vero assumenda nobis cum accusantium consectetur! Accusantium
+            velit ex eligendi quia nesciunt blanditiis, explicabo alias aliquam
+            quod consequatur deserunt beatae!
+          </p>
+          <span className="reg">Don't have an account?</span>
+          <button className="register">Register</button>
+        </div>
+        <div className="right">hello</div>
+      </div>
+    </div>
+  );
+};
+export default Login;
