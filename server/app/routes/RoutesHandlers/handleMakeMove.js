79c63748286426a2c3edc0ed89ff09480f271ed3
Mo'men Gharib <momengharib22@hotmail.com>
2023-07-10 15:02:03 +0300

    update endpoints

--- server/app/routes/RoutesHandlers/handleMakeMove.js
+++ server/app/routes/RoutesHandlers/handleMakeMove.js
@@ -1,5 +1,64 @@
-const handleMakeMove = (req, res) => {
-  
+const db = require("../../models");
+const Game = db.Game;
+const GP = db.GamePlayer;
+const handleMakeMove = async (req, res) => {
+  let gameID = req.body.gameID;
+  let dice = 1;
+  let game = await Game.findOne({
+    where: {
+      Id: gameID,
+    },
+  });
+
+  try {
+    if (game) {
+      // newGame = {...game};
+      dice = makeMove();
+
+      const currentPlayer = game.currentPlayer;
+      const gp = await GP.findOne({
+        where: { gameID: gameID, playerId: currentPlayer },
+      });
+      const oldPosition = gp.lastPosition;
+      const currentOrder = gp.order;
+      const nextOrder = (currentOrder + 1) % game.capacity;
+
+      const nextGp = await GP.findOne({
+        where: { gameID: gameID, order: nextOrder },
+      });
+
+      await GP.update(
+        {
+          lastPosition: Math.min(99, dice + oldPosition),
+        },
+        {
+          where: { playerId: currentPlayer },
+        }
+      );
+      await Game.update(
+        {
+          currentPlayer: nextGp.playerId,
+        },
+        {
+          where: { gameID: gameID },
+        }
+      );
+      if (dice + oldPosition >= 99) {
+        await Game.update(
+          {
+            status: "FINISHED",
+          },
+          {
+            where: { gameID: gameID },
+          }
+        );
+      }
+    } else {
+      throw new Error("Game doesn't exist");
+    }
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = handleMakeMove;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const auth = require("./middlewares/auth");
+const handleMakeMove = require("./app/routes/RoutesHandlers/handleMakeMove");
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
@@ -15,6 +17,23 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+const handleRegister = require("./app/routes/RoutesHandlers/handleRegister.js");
+const handleLogin = require("./app/routes/RoutesHandlers/handleLogin.js");
+const handleRetrieveGames = require("./app/routes/RoutesHandlers/handleRetrieveGames.js");
+const handleGetGames = require("./app/routes/RoutesHandlers/handleGetGames.js");
+const handleJoinGame = require("./app/routes/RoutesHandlers/handleJoinGame.js");
+const handleLeaveGame = require("./app/routes/RoutesHandlers/handleLeaveGame.js");
+const handleCreateGame = require("./app/routes/RoutesHandlers/handleCreateGame.js");
+
+//app.post("/makeMove", auth, handleMakeMove);
+app.post("./login", handleLogin);
+//router.route("/register").post(handleRegister);
+//router.route("/login").post(handleLogin);
+/*router.route("/retrieveGames").get(handleRetrieveGames);
+router.route("/getGame").get(handleGetGames);
+router.route("/joinGame").post(handleJoinGame);
+router.route("/leaveGame").post(handleLeaveGame);
+router.route("/createGame").post(handleCreateGame);*/
 const db = require("./app/models");
 
 // db.sequelize.sync()
