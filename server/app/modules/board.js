57a12406de79f129f8bdfab867fc352ccc648dd7
unknown <sherin.reda15@gmail.com>
2023-07-10 15:59:25 +0300

    leave-game

--- server/app/modules/board.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Board {
-    constructor(boardID, boardImage, snakes, ladders) {
-        this.boardID = boardID;
-        this.boardImage = boardImage;
-        this.snakes = snakes;
-        this.ladders = ladders;
-    }
-}
--- server/app/routes/RoutesHandlers/handleLeaveGame.js
+++ server/app/routes/RoutesHandlers/handleLeaveGame.js
@@ -1,5 +1,24 @@
-const handleLeaveGame = (req, res) => {
-  
+const db = require("../../models");
+const GamePlayer = db.GamePlayer;
+const Game = db.Game;
+const handleLeaveGame = async (req, res, next) => {
+  const { playerId, gameId } = req.body;
+  try {
+    let player = GamePlayer.findOne({ where: { playerId, gameId } });
+    if (!player) throw new Error("Player Not in The Game");
+    let game = await Game.findByPk(gameId);
+    if (!game) throw new Error("Game Doesn't Exist");
+    GamePlayer.destroy({ where: { playerId } });
+    let num = await Game.update(
+      { playesNumber: game.playesNumber - 1 },
+      { where: { Id: gameId } }
+    );
+    console.log(num);
+    if (num != 1) throw new Error("Can't Update Game");
+    res.status(200).send("Done");
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = handleLeaveGame;
