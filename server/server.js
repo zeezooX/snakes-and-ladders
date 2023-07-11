bdb27aed045ed35a70e47447afe74702a9bfc8f8
Ziyad Eslam <ziyadeslam@hotmail.com>
2023-07-11 14:55:31 +0300

    ..

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const socketIO = require("socket.io")(http, {
     origin: "*",
   },
 });
-const fetchTurn = require('./app/socket')
+//const fetchTurn = require('./app/socket')
 socketIO.use(socketAuth).on('connection', (socket) => {
   console.log(`${socket.id} just connected!`);
   socket.on('turn-load', (gameId, callback) => {
