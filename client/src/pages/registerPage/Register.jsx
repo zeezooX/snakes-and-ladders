53ece3268911b3979c8fd267eab66149064b6b24
Mo'men Gharib <momengharib22@hotmail.com>
2023-07-11 11:58:34 +0300

    update frontend

--- client/src/pages/registerPage/Register.jsx
+++ client/src/pages/registerPage/Register.jsx
@@ -1,3 +1,76 @@
-export default function Register() {
-  return <div>Register</div>;
-}
+import "./Register.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const navigate = useNavigate();
+  const [person, setPerson] = useState({
+    userName: "",
+    password: "",
+  });
+  const handleUser = (e) => {
+    const user = { ...person };
+    user[e.target.id] = e.target.value;
+    setPerson(user);
+  };
+
+  const handleSubmit = () => {
+    const send = async () => {
+      let Isvalid = await axios.post(`/login`, person);
+      if (Isvalid?.data) {
+        sessionStorage.setItem("authenticated", Isvalid?.data?.token);
+        navigate(`/`);
+      } else {
+        alert("Wrong credentials");
+      }
+    };
+    send();
+  };
+  console.log(person);
+  return (
+    <>
+      <div className="Register">
+        <div className="card">
+          <div className="left">
+            <h1>Snakes and Ladders</h1>
+            <p className="text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
+              omnis, vero assumenda nobis cum accusantium consectetur!
+              Accusantium velit ex eligendi quia nesciunt blanditiis, explicabo
+              alias aliquam quod consequatur deserunt beatae!
+            </p>
+            <span className="log">Do you have an account?</span>
+            <Link to="/login">
+              <button className="login">Login</button>
+            </Link>
+          </div>
+          <div className="right">
+            <h1 className="register">Register</h1>
+            <form>
+              <input
+                id="userName"
+                onChange={(e) => {
+                  handleUser(e);
+                }}
+                type="text"
+                placeholder="username"
+              />
+              <input
+                id="password"
+                onChange={(e) => {
+                  handleUser(e);
+                }}
+                type="password"
+                placeholder="password"
+              />
+              <button onClick={handleSubmit}>register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Register;
