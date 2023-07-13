519abdd871bbeb2e6328b29be7a3c8948ed640ac
3amo-magdy <amoramagdymahmoud@gmail.com>
2023-07-13 16:23:30 +0300

    Update handleLeaveGame.js

--- server/app/routes/RoutesHandlers/handleLeaveGame.js
+++ server/app/routes/RoutesHandlers/handleLeaveGame.js
@@ -2,79 +2,54 @@ const db = require("../../models");
 const fetchTurn = require("../../socket/handlers/fetchTurn");
 const GamePlayer = db.GamePlayer;
 const Game = db.Game;
+const Op = db.Sequelize.Op;
+
 require("dotenv").config()
 
 const handleLeaveGame = (socket) => {
-return async (req, res, next) => {
-  const playerId = req.user.userId
-  const {gameId} = req.body;
-  try {
-    let player = await GamePlayer.findOne({ where: { playerId, gameId } });
-    if (!player) throw new Error("Player Not in The Game");
-    let game = await Game.findByPk(gameId);
-    if (!game) throw new Error("Game Doesn't Exist");
-
-    if(game.currentPlayer === parseInt(playerId)){
-      
-      // set currentPlayer to the player with order++
-      const currentOrder = player.order;
-      const nextOrder = (currentOrder % game.playersNumber) + 1;
-      const nextGp = await GamePlayer.findOne({
-        where: { gameID: gameId, order: nextOrder },
-      });
-      if(!nextGp) throw new Error("Can't find next player in order")
+  return async (req, res, next) => {
+    const playerId = req.user.userId
+    const { gameId } = req.body;
+    try {
+      let player = await GamePlayer.findOne({ where: { playerId, gameId } });
+      if (!player) throw new Error("Player Not in The Game");
+      let game = await Game.findByPk(gameId);
+      if (!game) throw new Error("Game Doesn't Exist");
       await Game.update(
-        {
-          currentPlayer: nextGp.playerId,
-        },
-        {
-          where: { Id: gameId },
-        }
-      );
-    }
-    let num = await Game.update(
-      { playersNumber: game.playersNumber - 1 },
-      { where: { Id: gameId } }
-    );
-    if (num != 1) throw new Error("Can't Update Game");
-    await GamePlayer.destroy({ where: { playerId } });
-    let gamePlayers = await GamePlayer.findAll({ where: { gameId } });
-    for(const playerr of gamePlayers){
-      console.log(player.order);
-      if (playerr.order > player.order) {
-        console.log(playerr.order, player.order);
-        await GamePlayer.update(
-          { order: playerr.order - 1 },
-          { where: { Id: gameId, playerId: playerr.playerId } }
-        );
-      }
-    }
-    // gamePlayers.forEach((playerr) => {
-    //   if (playerr.order > player.order) {
-    //     console.log(playerr.order, player.order);
-    //     GamePlayer.update(
-    //       { order: playerr.order - 1 },
-    //       { where: { Id: gameId, playerId: playerr.playerId } }
-    //     );
-    //   }
-    // });
-    if (gamePlayers.length <= 1 && game.status.toLowerCase()!="pending") {
-      let num = await Game.update(
-        { status: "finished" },
+        { playersNumber: game.playersNumber - 1 },
         { where: { Id: gameId } }
       );
-      GamePlayer.destroy({where:{gameId:gameId}})
-      if (num != 1) throw new Error("Can't Update Game");
-    }
-    res.status(200).send("Done");
+      // if (num != 1) throw new Error("Can't Update Game");
+      const ord = player.ord
+      await GamePlayer.decrement(
+        { order: 1 },
+        {
+          where: {
+            Id: gameId, order: {
+              [Op.gt]: ord
+            }
+          }
+        }
+        )
+        
+      let gamePlayers = await GamePlayer.findAll({ where: { gameId } });
+      await GamePlayer.destroy({ where: { playerId } });
+      if (gamePlayers.length == 0) {
+        let num = await Game.update(
+          { status: "finished" },
+          { where: { Id: gameId } }
+        );
+        // if (num != 1) throw new Error("Can't Update Game");
+      }
+      res.status(200).send("Done");
 
-    fetchTurn(gameId).then((data)=>{
-      socket.in(process.env.ROOMPREFIX+String(gameId)).emit('room-update', data)
-    });
-  } catch (e) {
-    next(e);
+      fetchTurn(gameId).then((data) => {
+        socket.in(process.env.ROOMPREFIX + String(gameId)).emit('room-update', data)
+      });
+    } catch (e) {
+      next(e);
+    }
   }
-}
 };
 
 module.exports.create = handleLeaveGame;
